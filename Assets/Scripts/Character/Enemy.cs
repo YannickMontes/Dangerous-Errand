653c59108b2c2243eb70cc8c251b1a020233c383
YannickMontes <montes.yannick@gmail.com>
2020-04-18 16:57:35 +0200

    Basic heal

--- Assets/Scripts/Character/Enemy.cs
+++ Assets/Scripts/Character/Enemy.cs
@@ -10,23 +10,29 @@ public class Enemy : Character
 	public void DecreaseContamination(int value)
 	{
 		m_contamination -= value;
+		if (m_contamination <= 0)
+		{
+			m_animator.SetBool("Healed", true);
+		}
 	}
 
 	#region Private
 
 	protected override void Awake()
 	{
+		base.Awake();
 		m_animator = GetComponent<Animator>();
 		StartCoroutine(Shoot());
 	}
 
 	private IEnumerator Shoot()
 	{
-		while (true)
+		yield return new WaitForSeconds(Asset.TimeBetweenShoot);
+		while (m_contamination > 0)
 		{
-			yield return new WaitForSeconds(Asset.TimeBetweenShoot);
 			Projectile projectile = Projectile.AcquireInstance(Asset.DefaultProjectile, null, transform.position, Vector2.down);
 			m_animator.SetTrigger("Shoot");
+			yield return new WaitForSeconds(Asset.TimeBetweenShoot);
 		}
 	}
 
