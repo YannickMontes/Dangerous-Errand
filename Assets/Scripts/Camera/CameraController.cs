d493b6ca24dccccc18c19787265da14a2ca25d75
PapyKent <kentdu38@gmail.com>
2020-04-18 14:52:58 +0200

    Revert "Merge branch 'master' of https://github.com/YannickMontes/Dangerous-Errand"
    This reverts commit 2855ce74977a74d08558e23edf35cb292d80abdd, reversing
    changes made to 15841dbe109c118625f27617c9026f2209df9607.

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-	#region Private
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
-	private void Awake()
-	{
-		m_currentSpeed = m_defaultSpeed;
-	}
-
-	private void FixedUpdate()
-	{
-		transform.Translate(new Vector2(0.0f, m_currentSpeed));
-	}
-
-	[SerializeField]
-	private float m_defaultSpeed = 0.05f;
-
-	[NonSerialized]
-	private float m_currentSpeed = 0.0f;
-
-	#endregion Private
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
