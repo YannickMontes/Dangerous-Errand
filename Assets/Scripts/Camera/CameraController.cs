95099b53d4696260536ca31ba01e79fa9ef67e65
YannickMontes <montes.yannick@gmail.com>
2020-04-18 14:34:48 +0200

    CameraController V1

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	#region Private
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Awake()
+	{
+		m_currentSpeed = m_defaultSpeed;
+	}
+
+	private void FixedUpdate()
+	{
+		transform.Translate(new Vector2(0.0f, m_currentSpeed));
+	}
+
+	[SerializeField]
+	private float m_defaultSpeed = 0.05f;
+
+	[NonSerialized]
+	private float m_currentSpeed = 0.0f;
+
+	#endregion Private
 }
