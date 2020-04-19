0be1e1ce825296868191f1af16f50943ba11f6cb
YannickMontes <montes.yannick@gmail.com>
2020-04-19 10:42:03 +0200

    Player Correctly constraint to camera

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -38,6 +38,33 @@ public class Player : Character
 		{
 			m_verticalSpeed = value * Asset.Speed;
 		}
+
+		if ((m_verticalSpeed < 0 && IsCollidingBorder("Bot"))
+			|| (m_verticalSpeed > 0 && IsCollidingBorder("Top")))
+		{
+			m_verticalSpeed = 0.0f;
+		}
+		if ((m_horizontalSpeed < 0 && IsCollidingBorder("Left"))
+			|| (m_horizontalSpeed > 0 && IsCollidingBorder("Right")))
+		{
+			m_horizontalSpeed = 0.0f;
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.collider.tag.Contains("Border"))
+		{
+			m_borderColliding.Add(collision.gameObject);
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.collider.tag.Contains("Border"))
+		{
+			m_borderColliding.Remove(collision.gameObject);
+		}
 	}
 
 	private void OnShootButtonDown()
@@ -54,6 +81,7 @@ public class Player : Character
 	{
 		if (m_horizontalSpeed != 0 || m_verticalSpeed != 0)
 		{
+			Debug.Log("Translate x: " + m_horizontalSpeed + " Translate y: " + m_verticalSpeed);
 			transform.Translate(new Vector2(m_horizontalSpeed, m_verticalSpeed));
 		}
 	}
@@ -64,6 +92,18 @@ public class Player : Character
 		m_canShoot = true;
 	}
 
+	private bool IsCollidingBorder(string borderType)
+	{
+		foreach (GameObject border in m_borderColliding)
+		{
+			if (border.tag == $"{borderType}Border")
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
 	[NonSerialized]
 	private float m_horizontalSpeed = 0.0f;
 
@@ -72,6 +112,8 @@ public class Player : Character
 
 	[NonSerialized]
 	private bool m_canShoot = true;
+	[NonSerialized]
+	private List<GameObject> m_borderColliding = new List<GameObject>();
 
 	#endregion Private
 }
