33c18d0ced68a9bedfdd4ce3c2968422b873b465
YannickMontes <montes.yannick@gmail.com>
2020-04-19 14:38:04 +0200

    Broken shield on hit enemy

---
+++ Assets/Scripts/PowerUps/Shield.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+	public void BreakShield()
+	{
+		if (!m_isBroken)
+		{
+			m_animator.SetTrigger("Break");
+			m_isBroken = true;
+		}
+	}
+
+	//Trigger by anim event
+	public void Disable()
+	{
+		gameObject.SetActive(false);
+	}
+
+	#region Private
+
+	private void Awake()
+	{
+		m_animator = GetComponent<Animator>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Enemy")
+		{
+			BreakShield();
+		}
+	}
+
+	[NonSerialized]
+	private Animator m_animator = null;
+	[NonSerialized]
+	private bool m_isBroken = false;
+
+	#endregion Private
+}
